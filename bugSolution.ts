727ac0b642ae052e4e8e0bea4ad18e95e6304222
BugHunter-X <askzenai@gmail.com>
2025-02-20 05:32:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,9 @@
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 3, y: 7 });
+
+//Correct usage.  Passing a proper object.
+//No error will be thrown.
